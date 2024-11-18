8ae3a84424c3a6b7591b0504bd9183a688c1f02e
Vladislav Chesnov <tazaqsp@mail.ru>
2024-11-18 18:41:02 +0500

    approved to stage

---
+++ API/API.py
@@ -0,0 +1,30 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+CU_TOKEN = os.getenv('CU_TOKEN')
+WORKSPACE_ID = os.getenv('WORKSPACE_ID')
+
+
+def get_tasks_from_list(list_id, params=None):
+    url = f'https://api.clickup.com/api/v2/list/{list_id}/task'
+    headers = {
+        'Authorization': CU_TOKEN,
+    }
+    response = requests.get(url, headers=headers, params=params)
+    return response.json()['tasks']
+
+
+def update_task_status(task_id, status):
+    url = f'https://api.clickup.com/api/v2/task/{task_id}'
+    headers = {
+        'Authorization': CU_TOKEN,
+        'Content-Type': 'application/json',
+    }
+    data = {
+        'status': status
+    }
+    response = requests.put(url, headers=headers, json=data)
+    return response.json()
---
+++ API/Utils.py
@@ -0,0 +1,16 @@
+from API.API import get_tasks_from_list
+
+APPROVED = 'approved'
+STAGE = 'stage'
+BEFOREPROD = 'beforeprod'
+PRODUCTION = 'production'
+
+
+def get_tasks_from_lists(*list_ids, params=None) -> []:
+    print(list_ids)
+    lists_tasks = []
+    for list_id in list_ids:
+        current_list_tasks = get_tasks_from_list(list_id, params)
+        lists_tasks.extend(current_list_tasks)
+
+    return [task['id'] for task in lists_tasks]
