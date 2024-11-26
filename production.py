8b3fb79b9b9a0ed0cfdabf0d06a93018ddb70bd5
Vladislav Chesnov <tazaqsp@mail.ru>
2024-11-26 12:37:08 +0500

    Красивый вывод задач для production 2

--- production.py
+++ production.py
@@ -1,16 +1,16 @@
 import sys
 
 from API.API import update_task_status, add_comment_to_task
-from API.Utils import get_tasks_from_lists, BEFOREPROD, PRODUCTION, print_task_ids
+from API.Utils import get_tasks_from_lists, BEFOREPROD, PRODUCTION, print_tasks
 
 if __name__ == '__main__':
     comment, *list_ids = sys.argv[1:]
-    task_ids = get_tasks_from_lists(*list_ids, params={
+    tasks = get_tasks_from_lists(*list_ids, is_production=True, params={
         'statuses[]': [BEFOREPROD],
     })
 
-    for task_id in task_ids:
-        update_task_status(task_id, PRODUCTION)
-        add_comment_to_task(task_id, comment)
+    for task in tasks:
+        update_task_status(task['id'], PRODUCTION)
+        add_comment_to_task(task['id'], comment)
 
-    print_task_ids(task_ids)
+    print_tasks(tasks)
